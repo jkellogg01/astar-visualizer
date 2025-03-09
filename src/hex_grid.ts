d45d29158859b8196c021b76e2ab229c67ac974a
Joshua Kellogg <kellogg.20.joshua@gmail.com>
2025-03-08 20:33:34 -0700

    hexagon snaps to grid while following cursor

--- src/hex_grid.ts
+++ src/hex_grid.ts
@@ -1,5 +1,24 @@
 import { Vertex } from "./geometry";
 
+export class Axial_Coordinate {
+  q: number;
+  r: number;
+
+  constructor(q: number, r: number) {
+    const q_grid = Math.round(q);
+    const q_delta = q - q_grid;
+    const r_grid = Math.round(r);
+    const r_delta = r - r_grid;
+    if (Math.abs(q_delta) >= Math.abs(r_delta)) {
+      this.r = r_grid;
+      this.q = q_grid + Math.round(q_delta + 0.5 * r_delta);
+    } else {
+      this.q = q_grid;
+      this.r = r_grid + Math.round(r_delta + 0.5 * q_delta);
+    }
+  }
+}
+
 export class Hex_Grid {
   hex_width: number;
   hex_height: number;
@@ -9,9 +28,31 @@ export class Hex_Grid {
     const angle_rad = (angle * Math.PI) / 180;
     this.hex_height = Math.sqrt(3) * radius * Math.cos(angle_rad);
   }
+
+  pixel_to_axial(x: number, y: number) {
+    const q = (4 / (3 * this.hex_width)) * x;
+    const r = (-2 / (3 * this.hex_width)) * x + (1 / this.hex_height) * y;
+    return new Axial_Coordinate(q, r);
+  }
+
+  axial_to_pixel(q: number, r: number) {
+    const x = 0.75 * this.hex_width * q;
+    const y = 0.5 * this.hex_height * q + this.hex_height * r;
+    return { x, y } as Vertex;
+  }
+
+  hex_path(q: number, r: number) {
+    const pixel_center = this.axial_to_pixel(q, r);
+    return hex_path(
+      pixel_center.x,
+      pixel_center.y,
+      this.hex_width,
+      this.hex_height,
+    );
+  }
 }
 
-export function hex_path(
+function hex_path(
   center_x: number,
   center_y: number,
   width: number,
@@ -31,9 +72,9 @@ export function hex_path(
 
   let result = new Path2D();
 
-  result.moveTo(vertices[0].x, vertices[0].y)
-  for ( let i = 1; i < vertices.length; i += 1) {
-    result.lineTo(vertices[i].x, vertices[i].y)
+  result.moveTo(vertices[0].x, vertices[0].y);
+  for (let i = 1; i < vertices.length; i += 1) {
+    result.lineTo(vertices[i].x, vertices[i].y);
   }
   result.closePath();
 
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { hex_path } from "./hex_grid";
+import { Hex_Grid } from "./hex_grid";
 import "./reset.css";
 import "./style.css";
 
@@ -6,6 +6,8 @@ let cursor = {
   x: innerWidth / 2,
   y: innerHeight / 2,
 };
+
+const grid = new Hex_Grid(100, 45);
 
 const app_root = document.querySelector<HTMLDivElement>("#app");
 if (!app_root) {
@@ -40,8 +42,14 @@ function set_canvas_size() {
 
 function draw() {
   requestAnimationFrame(draw);
+  if (!ctx) {
+    return;
+  }
 
-  ctx?.clearRect(0, 0, innerWidth, innerHeight);
-  const hex = hex_path(cursor.x, cursor.y, 100, 50);
-  ctx?.stroke(hex);
+  ctx.clearRect(0, 0, innerWidth, innerHeight);
+  const cursor_grid_coord = grid.pixel_to_axial(cursor.x, cursor.y);
+  
+  ctx.fillStyle = "rgb(200 200 0 / 50%)";
+  const hex = grid.hex_path(cursor_grid_coord.q, cursor_grid_coord.r);
+  ctx.fill(hex);
 }
